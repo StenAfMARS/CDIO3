b8b0b34abb56bbebba739bf51380227f7d6d612d
Shania Hau <shaniahau@gmail.com>
2019-11-12 15:29:39 +0100

    prøvet at implementere gameflow, gamelogic, player chancecard. kunne ikke benytte peters die og dicecarrier, så har anvendt den gamle klasse dices fra cdio2

--- src/main/java/ChanceCard.java
+++ src/main/java/ChanceCard.java
@@ -2,37 +2,50 @@ import java.util.Random;
 
 public class ChanceCard {
 
-    private void chanceaction(String[] chance){
+    String[] ch = {"ch1", "ch2", "ch3", "ch4",
+            "ch5", "ch6", "ch7", "ch8", "ch9", "ch10", "ch11",
+            "ch12", "ch13", "ch14", "ch15", "ch16", "ch17", "ch18",
+            "skibet", "bilen", "hunden", "katten", "ch19", "ch20"};
 
-        String[] ch = {"ch1", "ch2", "ch3", "ch4",
-                "ch5", "ch6", "ch7", "ch8", "ch9", "ch10", "ch11",
-                "ch12", "ch13", "ch14", "ch15", "ch16", "ch17", "ch18",
-                "skibet", "bilen", "hunden", "katten", "ch19", "ch20"};
+    public String drawCard(){
+
         Random r = new Random();
         int randomNumber = r.nextInt(ch.length);
         System.out.println(ch[randomNumber]);
 
-        switch (ch){
+        return ch[randomNumber];
+    }
+
+    private void cardEffect(){
+
+        //some psudocode in the switch case
+        switch (drawCard()){
             case "ch1":
 
                 break;
             case "ch2":
-
+            //currentplayer.posision = start[];
+            //currentplayer.getStartmoney;
                 break;
             case "ch3":
-
+            //scanner
+            //currentplayer.posision = posision[]+input.nextInt;
                 break;
             case "ch4":
 
                 break;
             case "ch5":
-
+                /*System.out.println("vælg???");
+                if(input.equals(1))
+                    currentplayer.posistion = posision[]+1;
+                else
+                    try again?????*/
                 break;
             case "ch6":
 
                 break;
             case "ch7":
-
+            //currentplayer.setMoney = getMoney -2;
                 break;
             case "ch8":
 
---
+++ src/main/java/Dices.java
@@ -0,0 +1,18 @@
+public class Dices {
+
+    int sides;
+
+    int faceValue1;
+    int faceValue2;
+
+    //Method that rolls 2 dices and set Dices equal to the to values sums
+    public Dices(int sides) {
+        this.sides = sides;
+    }
+
+    public int roll() {
+        faceValue1 = (int) (Math.random() * sides + 1);
+        faceValue2 = (int) (Math.random() * sides + 1);
+        return faceValue1 + faceValue2;
+    }
+}

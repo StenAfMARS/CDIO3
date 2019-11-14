c2037c1b4a563b1c6c4797e09e0fc80c6db9c0a2
Boosted-Sama <s195485@student.dtu.dk>
2019-11-14 10:10:44 +0100

    GUI som jeg har brugt ud fra maven repo som vi har fået afvide vi skal. Jeg har lagt nogle åbene variabler som vi kan bruge til de andre klasser

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import GUI_Full.GUI;
+
 public class Main {
 
     public static void main(String[] args) {
 
         DiceCarrier dc = DiceCarrier.getInstance();
 
+        GUI gui = new GUI();
+
+        gui.getFields();
 
         System.out.println();
     }
